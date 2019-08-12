15f8e30e69934cf2f9df4173b343e631ccb10ae9
CT <christ@kagr.com>
2019-08-12 10:30:39 -0400

    fixing travis failures

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -6,8 +6,8 @@ test_that('get_data can take single or multiple countries as input',{
   one_country<-eq_get_data(filename, country = 'USA', start_date = '2001-01-01', end_date = '2003-01-01')
   two_countries<-eq_get_data(filename, country = c('MEXICO','USA'), start_date = '2001-01-01', end_date = '2003-01-01')
 
-  expect_true(one_country %>% select(COUNTRY) %>% unique()%>% nrow() ==1)
-  expect_true(two_countries %>% select(COUNTRY) %>% unique()%>% nrow() ==2)
+  expect_true(one_country %>% dplyr::select(COUNTRY) %>% unique()%>% nrow() ==1)
+  expect_true(two_countries %>% dplyr::select(COUNTRY) %>% unique()%>% nrow() ==2)
 
 
 } )
--- tests/testthat/test-map.R
+++ tests/testthat/test-map.R
@@ -12,18 +12,18 @@ test_that('eq_map returns a leaflet map when given valid input', {
 test_that('eq_create_label only returns non-NA labels and data', {
   filename <- system.file('extdata', 'earthquakes.tsv.gz', package = 'NOAA')
   data<- eq_get_data(filename, country = 'MEXICO', start_date = '2006-09-09')
-  data<- data%>%select(DATE, LOCATION_NAME, EQ_PRIMARY, TOTAL_DEATHS)
+  data<- data%>% dplyr::select(DATE, LOCATION_NAME, EQ_PRIMARY, TOTAL_DEATHS)
 
   # 'Location' should not be in label if LOCATION_NAME data is NA
-  NA_LOCATION_NAME <- data %>% filter(DATE=='2006-09-10') %>% eq_create_label()
+  NA_LOCATION_NAME <- data %>% dplyr::filter(DATE=='2006-09-10') %>% eq_create_label()
   expect_true(stringr::str_count(NA_LOCATION_NAME, 'Location') ==0)
 
   # 'Total deaths' should not be in label if TOTAL_DEATHS data is NA
-  NA_TOTAL_DEATHS <- data %>% filter(DATE=='2008-02-09') %>% eq_create_label()
+  NA_TOTAL_DEATHS <- data %>% dplyr::filter(DATE=='2008-02-09') %>% eq_create_label()
   expect_true(stringr::str_count(NA_TOTAL_DEATHS, 'Total deaths') ==0)
 
   # 'Date', 'Location', 'Total deaths', and 'Magnitude', should all be in label is no NAs
-  NO_NA <- data %>% filter(DATE=='2017-09-08') %>% eq_create_label()
+  NO_NA <- data %>% dplyr::filter(DATE=='2017-09-08') %>% eq_create_label()
   expect_true(stringr::str_count(NO_NA, 'Total deaths') ==1)
   expect_true(stringr::str_count(NO_NA, 'Location') ==1)
   expect_true(stringr::str_count(NO_NA, 'Date') ==1)
